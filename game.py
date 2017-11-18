01d98f4165ae049ffbb8223ad8d7657812cb7eed
Rommel Lasso <soccerply@msn.com>
2017-11-18 01:26:29 -0500

    Adding Initial Game File

---
+++ game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assignemnt Week Seven - Pig Game"""
+
+def main():
+    # Call the Game Class to begin Game
+
+    print 'Welcome to Our Game'
+
+
+if __name__ == '__main__':
+    main()
